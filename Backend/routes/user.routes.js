7aae92436145d3015ed68a87962cefcea3e9eb96
akramcodez <skcodewizard786@gmail.com>
2025-03-08 12:19:56 +0530

    user lgin route

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -4,15 +4,29 @@ const userModel = require('../models/user.model');
 const { body } = require('express-validator');
 const userController = require('../controllers/user.controller');
 
-router.post('/register', [
-  body('email').isEmail().withMessage('Invalid Email'),
-  body('fullname.firstname')
-    .isLength({ min: 3 })
-    .withMessage('First Name is required'),
-  body('password')
-    .isLength({ min: 6 })
-    .withMessage('Password must be atleast 6 characters long'),
+router.post(
+  '/register',
+  [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('fullname.firstname')
+      .isLength({ min: 3 })
+      .withMessage('First Name is required'),
+    body('password')
+      .isLength({ min: 6 })
+      .withMessage('Password must be atleast 6 characters long'),
+  ],
   userController.registerUser,
-]);
+);
+
+router.post(
+  '/login',
+  [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password')
+      .isLength({ min: 6 })
+      .withMessage('Password must be atleast 6 characters long'),
+  ],
+  userController.loginUser,
+);
 
 module.exports = router;
